2386396b3f3a3da0e440b899502caeed73646510
EmptyPuff <bernardo.mira98@gmail.com>
2021-04-20 18:44:31 +0100

    Project creation

---
+++ Cri_Art/Cri_Art/src/main/kotlin/pt/isel/leic/ps/g42/Cri_Art/CriArtApplication.kt
@@ -0,0 +1,11 @@
+package pt.isel.leic.ps.g42.Cri_Art
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CriArtApplication
+
+fun main(args: Array<String>) {
+	runApplication<CriArtApplication>(*args)
+}
---
+++ Cri_Art/Cri_Art/src/test/kotlin/pt/isel/leic/ps/g42/Cri_Art/CriArtApplicationTests.kt
@@ -0,0 +1,13 @@
+package pt.isel.leic.ps.g42.Cri_Art
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CriArtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
