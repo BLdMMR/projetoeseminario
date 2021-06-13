30928bc9899f1cc921d31f8b2fa32886e40594f7
unknown <cristianclefos@mail.com>
2021-06-13 16:32:36 +0100

    attempt to fix https

--- Cri_Art_backend/build.gradle.kts
+++ Cri_Art_backend/build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.data:spring-data-elasticsearch:4.2.0")
+	implementation("org.springframework.boot:spring-boot-starter-security:2.5.0")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
---
+++ Cri_Art_backend/src/main/kotlin/pt/isel/leic/ps/g42/Cri_Art/configuration/WebSecurityConfig.kt
@@ -0,0 +1,20 @@
+package pt.isel.leic.ps.g42.Cri_Art.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+            .antMatchers("/**")
+            .permitAll()
+    }
+
+}
+
