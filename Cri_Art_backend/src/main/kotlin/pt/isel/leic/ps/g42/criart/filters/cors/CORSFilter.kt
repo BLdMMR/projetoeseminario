ef5d816b0029f8d233a7ca70a33f3516ec76f9ff
Clefos <cristianclefos@mail.com>
2021-09-26 22:21:52 +0100

    cors props

--- Cri_Art_backend/src/main/kotlin/pt/isel/leic/ps/g42/criart/filters/cors/CORSFilter.kt
+++ Cri_Art_backend/src/main/kotlin/pt/isel/leic/ps/g42/criart/filters/cors/CORSFilter.kt
@@ -1,5 +1,6 @@
 package pt.isel.leic.ps.g42.criart.filters.cors
 
+import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 import java.io.IOException
@@ -8,16 +9,17 @@ import java.util.logging.LogRecord
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-//
+
 //@Component
 //class CORSFilter : OncePerRequestFilter() {
 //    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
-//        response.addHeader("Access-Control-Allow-Origin", "*")
-//        response.addHeader("Access-Control-Allow-Headers",  "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
-//        response.addHeader("Access-Control-Allow-Credentials", "true")
-//        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH")
+//        if (request.method == HttpMethod.OPTIONS.name) {
+//            response.addHeader("Access-Control-Allow-Origin", "*")
+//            response.addHeader("Access-Control-Allow-Headers",  "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+//            response.addHeader("Access-Control-Allow-Credentials", "true")
+//            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH")
+//        }
 //
+//        filterChain.doFilter(request, response)
 //    }
-//
-//
 //}
