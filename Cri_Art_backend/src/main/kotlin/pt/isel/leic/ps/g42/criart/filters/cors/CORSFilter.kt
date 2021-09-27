3e72dc28d88630993d0d03428580397c2cbb812b
Clefos <cristianclefos@mail.com>
2021-09-27 23:30:31 +0100

    solved cors issues locally

--- Cri_Art_backend/src/main/kotlin/pt/isel/leic/ps/g42/criart/filters/cors/CORSFilter.kt
+++ Cri_Art_backend/src/main/kotlin/pt/isel/leic/ps/g42/criart/filters/cors/CORSFilter.kt
@@ -1,25 +1,39 @@
 package pt.isel.leic.ps.g42.criart.filters.cors
 
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
-import java.io.IOException
-import java.util.logging.Filter
-import java.util.logging.LogRecord
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-//@Component
-//class CORSFilter : OncePerRequestFilter() {
-//    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
-//        if (request.method == HttpMethod.OPTIONS.name) {
-//            response.addHeader("Access-Control-Allow-Origin", "*")
-//            response.addHeader("Access-Control-Allow-Headers",  "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
-//            response.addHeader("Access-Control-Allow-Credentials", "true")
-//            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH")
-//        }
-//
-//        filterChain.doFilter(request, response)
-//    }
-//}
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class CORSFilter : OncePerRequestFilter() {
+
+    private val corsHeaders = "Access-Control-Allow-Headers, " +
+            "Access-Control-Allow-Origin, " +
+            "Origin,"  +
+            "Accept, " +
+            "X-Requested-With, " +
+            "Content-Type, " +
+            "Access-Control-Request-Method, " +
+            "Access-Control-Request-Headers"
+
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+
+        response.addHeader("Access-Control-Allow-Origin", "*")
+        response.addHeader("Access-Control-Allow-Headers",  corsHeaders)
+        response.addHeader("Access-Control-Allow-Credentials", "true")
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH")
+
+        if (HttpMethod.OPTIONS.name == request.method) {
+            response.status = HttpServletResponse.SC_OK
+        } else {
+            filterChain.doFilter(request, response)
+        }
+    }
+}
